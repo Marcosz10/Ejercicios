a19aa2db654667f90919549c981ea1a9b0dcbcb8
arielcarrera <arielcarreram10@gmail.com>
2018-11-19 22:57:21 -0500

    Ejercicio N2

---
+++ src/com/marcosz10/vista/PruebaTiempo3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.marcosz10.vista;
+
+import com.marcosz10.modelo.Tiempo1;
+import com.marcosz10.modelo.Tiempo2;
+
+/**
+ *
+ * @author Flia. Carrera
+ */
+public class PruebaTiempo3 {
+    public static void main(String[] args) {
+        Tiempo2 t1=new Tiempo2();
+        Tiempo2 t2=new Tiempo2();
+        Tiempo2 t3=new Tiempo2();
+        Tiempo2 t4=new Tiempo2();
+        Tiempo2 t5=new Tiempo2();
+        Tiempo2 t6=new Tiempo2();
+    }
+    
+}
